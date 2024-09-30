47418196e541739b2ebd4279129ad092b3618469
salmanals <salman.al.salem727@gmail.com>
2024-09-30 09:47:19 +0300

    First Dart Task

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  double tempInFahrenheit = 86; 
+  double tempInCelsius = 32;
+  print("The weather in Celsius is ${(tempInFahrenheit-tempInCelsius)/1.8}"); 
+
+  print("The weather in Celsius is ${tempInCelsius*1.8+32}"); 
+
+
+
+
+
+}
